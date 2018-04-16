ae6ea7ed3d8400569a5d42d276fd59a7bdcd94ad
Jan Kožusznik <jan@kozusznik.cz>
2018-04-16 09:59:10 +0200

    fix: operator

--- src/test/FooConsole.java
+++ src/test/FooConsole.java
@@ -7,7 +7,7 @@ public class FooConsole {
 	public static void main(String[] args) {
 		Calculator calculator = new Calculator();
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter operand.");
+		System.out.println("Enter operator.");
 		char op = sc.next().charAt(0);
 		double operand1 = 0.;
 		double operand2 = 0.;
