7962ccbf7b104c98bd25429fc497e08d508d9dbf
Jan Kožusznik <jan@kozusznik.cz>
2018-04-16 09:46:43 +0200

    version

---
+++ src/test/Calculator.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * @version 0.0.1
+ * @author koz01
+ *
+ */
+public class Calculator {
+
+	public double doOperation(char operator, double operand1,double operand2) {
+		throw new UnsupportedOperationException();
+	}
+}
--- src/test/FooConsole.java
+++ src/test/FooConsole.java
@@ -1,8 +1,14 @@
 package test;
 
+import java.util.Scanner;
+
 public class FooConsole {
 
 	public static void main(String[] args) {
-		System.out.println("JVM is running.");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter operand.");
+		char op = sc.next().charAt(0);
+		
+	
 	}
 }
