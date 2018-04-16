770c60ef3b5eca9d3a3a713e1153298a703670dc
Jan Kožusznik <jan@kozusznik.cz>
2018-04-16 09:56:31 +0200

    feat: operationUsing

--- src/test/FooConsole.java
+++ src/test/FooConsole.java
@@ -5,10 +5,13 @@ import java.util.Scanner;
 public class FooConsole {
 
 	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter operand.");
 		char op = sc.next().charAt(0);
+		double operand1 = 0.;
+		double operand2 = 0.;
 		
-	
+		System.out.println("result is" + calculator.doOperation(op, operand1, operand2));
 	}
 }
